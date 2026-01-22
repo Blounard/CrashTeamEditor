6dd4c7c0f9ea1360acfb823a5132e1f02ae522fd
Blounard <Blounard@hotmail.fr>
2026-01-22 23:03:30 +0100

    Some BSP Fix
    Swapped left child and right child to match vanilla
    Assigned a value to BSPBranch.unk1 instead of the constant value.
    It more or less mimic what vanilla does.
    I assume this value correspond to the slice of the BSP branch between the left child and right child. On vanilla, it's always in between the bounding box values alongside the AxisSplit, but with an added division by 2 (this is why I take the midpoint of the Bbox, and divide by 2)

--- src/bsp.cpp
+++ src/bsp.cpp
@@ -258,15 +258,15 @@ float BSP::Split(std::vector<size_t>& left, std::vector<size_t>& right, const Ax
 		switch (axis)
 		{
 		case AxisSplit::X:
-			if (quad.GetCenter().x > midpoint) { right.push_back(index); }
+			if (quad.GetCenter().x < midpoint) { right.push_back(index); }
 			else { left.push_back(index); }
 			break;
 		case AxisSplit::Y:
-			if (quad.GetCenter().y > midpoint) { right.push_back(index); }
+			if (quad.GetCenter().y < midpoint) { right.push_back(index); }
 			else { left.push_back(index); }
 			break;
 		case AxisSplit::Z:
-			if (quad.GetCenter().z > midpoint) { right.push_back(index); }
+			if (quad.GetCenter().z < midpoint) { right.push_back(index); }
 			else { left.push_back(index); }
 			break;
 		}
@@ -316,6 +316,12 @@ std::vector<uint8_t> BSP::SerializeBranch() const
 	}
 	else { branch.rightChild = BSPID::EMPTY; }
 	branch.unk1 = 0xFF40;
+	switch (m_axis)
+	{
+	case AxisSplit::X: branch.unk1 = ConvertFloat((m_bbox.min.x + m_bbox.max.x) / 4, FP_ONE_GEO); break;
+	case AxisSplit::Y: branch.unk1 = ConvertFloat((m_bbox.min.y + m_bbox.max.y) / 4, FP_ONE_GEO); break;
+	case AxisSplit::Z: branch.unk1 = ConvertFloat((m_bbox.min.z + m_bbox.max.z) / 4, FP_ONE_GEO); break;
+	}
 	branch.unk2 = 0;
 	branch.unk3 = 0;
 	std::memcpy(buffer.data(), &branch, sizeof(branch));
