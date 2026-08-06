245cbc563a5773bb5aefbf9bac02abb7ee87e8fb
Blounard <Blounard@hotmail.fr>
2026-08-07 01:49:22 +0200

    CAMERA_SEARCH quadflag
    renamed collision trigger to camera search.
    this flag is what tell the game to use visible set from the quad.
    Should be used only on drivable stuff (no walls)
    
    Added this flag to vistree generation : there is no point having source quad for the lightray not having this flag.
    This remove the need for ground cast only too.

--- src/quadblock.h
+++ src/quadblock.h
@@ -30,18 +30,18 @@ struct QuadFlags
 	static constexpr uint16_t KICKERS_TWO = 1 << 8;
 	static constexpr uint16_t MASK_GRAB = 1 << 9;
 	static constexpr uint16_t TIGER_TEMPLE_DOOR = 1 << 10;
-	static constexpr uint16_t COLLISION_TRIGGER = 1 << 11;
+	static constexpr uint16_t CAMERA_SEARCH = 1 << 11;
 	static constexpr uint16_t GROUND = 1 << 12;
 	static constexpr uint16_t WALL = 1 << 13;
 	static constexpr uint16_t NO_COLLISION = 1 << 14;
 	static constexpr uint16_t INVISIBLE_TRIGGER = 1 << 15;
-	static constexpr uint16_t DEFAULT = GROUND | COLLISION_TRIGGER;
+	static constexpr uint16_t DEFAULT = GROUND | CAMERA_SEARCH;
 	static inline const std::unordered_map<std::string, uint16_t> LABELS = {
 		{"Invisible", INVISIBLE}, {"Moon Gravity", MOON_GRAVITY}, {"Reflection", REFLECTION},
 		{"Kickers (?)", KICKERS}, {"Out of Bounds", OUT_OF_BOUNDS}, {"Never Used (?)", NEVER_USED},
 		{"Trigger Script", TRIGGER_SCRIPT }, {"Reverb", REVERB}, {"Kickers Two (?)", KICKERS_TWO},
-		{"Mask Grab", MASK_GRAB }, {"Tiger Temple Door", TIGER_TEMPLE_DOOR}, {"Collision Trigger", COLLISION_TRIGGER},
-		{"Ground", GROUND}, {"Wall", WALL}, {"No Colision", NO_COLLISION}, {"Invisible Trigger", INVISIBLE_TRIGGER}
+		{"Mask Grab", MASK_GRAB }, {"Tiger Temple Door", TIGER_TEMPLE_DOOR}, {"Camera Search", CAMERA_SEARCH},
+		{"Ground", GROUND}, {"Wall", WALL}, {"No Collision", NO_COLLISION}, {"Invisible Trigger", INVISIBLE_TRIGGER}
 	};
 };
 
