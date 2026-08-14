b0c5375e14056b16ec818404898752baee94204a
Blounard <Blounard@hotmail.fr>
2026-08-14 14:28:35 +0200

    compil fix

--- src/quadblock.cpp
+++ src/quadblock.cpp
@@ -925,7 +925,7 @@ bool Quadblock::SnapPoint(Vec3& pos, Vec3& rot, const Vec3& projectDir, float ba
 		const Vec3& A = m_p[tri[0]].m_pos;
 		const Vec3& B = m_p[tri[1]].m_pos;
 		const Vec3& C = m_p[tri[2]].m_pos;
-		if (SnapTriangle(A, B, C, point, pos, rot, projectDir, barycentricTolerance))
+		if (SnapTriangle(A, B, C, pos, rot, projectDir, barycentricTolerance))
 			return true;
 	}
 	return false;
