ad6d7f36747b10d4662a87c57d6ccfd9aabbe147
Blounard <Blounard@hotmail.fr>
2026-09-15 17:28:26 +0200

    Fix blend mode
    Make sure blend mode is ADDITIVE_TRANSLUCENT for non transparent textures

--- src/texture.cpp
+++ src/texture.cpp
@@ -241,7 +241,11 @@ bool Texture::CreateTexture()
 {
 	int channels;
 	stbi_uc* image = stbi_load(m_path.string().c_str(), &m_width, &m_height, &channels, 0);
-	if (image == nullptr) { return false; }
+	if (image == nullptr)
+	{
+		printf("ERROR : CAN'T LOAD IMAGE AT %s\n", m_path.string().c_str());
+		return false;
+	}
 	bool alphaImage = channels == 4;
 	int semiTransparentPx = 0;
 	std::vector<size_t> colorIndexes;
@@ -260,10 +264,16 @@ bool Texture::CreateTexture()
 		if (!foundColor) { m_clut.push_back(color); }
 		colorIndexes.push_back(clutIndex);
 	}
-	m_semiTransparent = semiTransparentPx >= (pxCount / 2);
+	m_semiTransparent = semiTransparentPx > 0;
+	if (!m_semiTransparent && m_blendMode == PSX::BlendMode::HALF_TRANSPARENT)
+	{
+		m_blendMode = PSX::BlendMode::ADDITIVE_TRANSLUCENT;
+	}
+
 	Texture::BPP bpp = GetBPP();
 	if (GetVRAMWidth() > TEXPAGE_WIDTH || GetHeight() > TEXPAGE_HEIGHT)
 	{
+		printf("ERROR : TEXTURE TOO BIG\n");
 		stbi_image_free(image);
 		return false;
 	}
